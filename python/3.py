856b4978970bc7647aec8a6623d78b58f48b7e79
Low Jeng Lam <jenglamlow@gmail.com>
2016-06-15 10:29:47 +0200

    Update 3.py

--- python/3.py
+++ python/3.py
@@ -23,3 +23,34 @@ def max_prime_factor(n):
 t0 = clock()
 print(max_prime_factor(600851475143))
 print(clock() - t0)
+
+# Based on the solution given
+def max_prime_factor_solution(n):
+    last_factor = 0
+    if n % 2 == 0:
+        last_factor = 2
+        n = n / 2
+        while n % 2 == 0:
+            n = n / 2
+    else:
+        last_factor = 1
+
+    factor = 3
+    max_factor = sqrt(n)
+    while n > 1 and factor < max_factor:
+        if n % factor == 0:
+            n = n / factor
+            last_factor = factor
+            while n % factor == 0:
+                n = n / factor
+            max_factor = sqrt(n)
+        factor = factor + 2
+
+    if n == 1:
+        return last_factor
+    else:
+        return n
+
+t0 = clock()
+print(max_prime_factor_solution(600851475143))
+print(clock() - t0)
