fc038e5c099f78d9e9b98bb0eb091f7ec8300db6
Low Jeng Lam <jenglamlow@gmail.com>
2016-06-26 11:28:40 +0800

    Problem 18 & 67

---
+++ python/067.py
@@ -0,0 +1,25 @@
+
+M = []
+
+with open("p067_triangle.txt") as file:
+    for line in file:
+        line_list = [int(i) for i in line.split()]
+        M.insert(0, line_list)
+
+
+def answer():
+    sum_left = 0
+    sum_right = 0
+
+    while len(M) > 1:
+        temp = []
+        for j in range(len(M[0])-1):
+            sum_left = M[0][j] + M[1][j]
+            sum_right = M[0][j+1] + M[1][j]
+            temp.append(max(sum_left, sum_right))
+        del M[0:2]
+        M.insert(0, temp)
+
+    return(M[0][0])
+
+print(answer())
